cad0527c38fb6e11a1d41a58079d5b0b4bc81f38
Aleksandra <a0993715642@gmail.com>
2021-03-23 22:20:53 +0200

    home view

--- views/home.view.php
+++ views/home.view.php
@@ -1,1 +1,11 @@
-<p>Hi from home!</p>
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+    
+    <main class="px-3">
+        <h1>Make your life easier!</h1>
+        <p class="lead">E-Recoding</p>
+        <p class="lead">
+            <a href="#" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
+        </p>
+    </main>
+
+</div>
