1f96d83c8d8e7e5a448a5fcf12373dab16218af4
Aleksandra <a0993715642@gmail.com>
2021-03-23 21:43:50 +0200

    edit

--- bootstrap.php
+++ bootstrap.php
@@ -3,10 +3,12 @@
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/app/routing.php';
 
-echo __LINE__;
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+
 use App\Core\Classes\Router;
 
-var_dump(Router::$routing);
-$obj = new Router();
 
-echo $obj->run();
+$router = new Router();
+$router->run();
