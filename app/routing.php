d281dc20de315c7819457bbbc26757f2567e05c0
AleksandraHlukhova <a0993715642@gmail.com>
2021-03-25 16:29:37 +0200

    add new route

--- app/routing.php
+++ app/routing.php
@@ -3,4 +3,8 @@
 use App\Core\Classes\Router;
 
 Router::get('/', 'App\Classes\Controllers\MainController@index');
+Router::get('/signup', 'App\Classes\Controllers\RegisterController@index');
+Router::post('/signup', 'App\Classes\Controllers\RegisterController@signup');
 Router::get('/signin', 'App\Classes\Controllers\LoginController@index');
+Router::post('/signin', 'App\Classes\Controllers\LoginController@signin');
+
