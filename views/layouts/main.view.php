76c86d25c547e84aa6978c3e5dcd8071346995aa
AleksandraHlukhova <a0993715642@gmail.com>
2021-03-25 16:31:14 +0200

    edit route

--- views/layouts/main.view.php
+++ views/layouts/main.view.php
@@ -18,9 +18,9 @@
             <div>
                 <h3 class="float-md-start mb-0">E-Recoding</h3>
                 <nav class="nav nav-masthead justify-content-center float-md-end">
-                    <a class="nav-link active" aria-current="page" href="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>">Home</a>
+                    <a class="nav-link active" aria-current="page" href="/">Home</a>
                     <a class="nav-link" href="<?php echo htmlspecialchars($_SERVER['PHP_SELF'] . '?action=\signin') ?>">Signin</a>
-                    <a class="nav-link" href="#">Signup</a>
+                    <a class="nav-link" href="/signup">Signup</a>
                 </nav>
             </div>
         </header>
--- views/login.view.php
+++ views/login.view.php
@@ -1,6 +1,6 @@
 <div class="text-center">
     <main class="form-signin">
-        <form>
+        <form action="\signin" method="POST">
             <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
             <label for="inputEmail" class="visually-hidden">Email address</label>
             <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
