46dbf2b1ddeb5a02f68ac33a63a0493ed16ef38b
Aleksandra <a0993715642@gmail.com>
2021-04-07 21:55:04 +0300

    create login controller

--- api/classes/controllers/LoginController.php
+++ api/classes/controllers/LoginController.php
@@ -2,23 +2,54 @@
 
 namespace Api\Classes\Controllers;
 use Api\Classes\Controllers\Controller;
+use Api\Core\Classes\Request;
+use Api\Classes\Requests\LoginRequest;
 
-class RegisterController extends Controller {
+class LoginController extends Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        if($this->request->postMethod())
+        {
+            $this->request = new LoginRequest();
+        }
+    }
 
     /**
      * 
      */
     public function index()
     {
-        //
+        echo 'hi';
     }
 
     /**
      * post request
-     * handle sign in
+     * handle sign up
      */
-    public function signin()
+    public function signin(Request $request)
     {
-        var_dump($this->request->input());
+        // $erros = $this->request->validate->errors;
+        // if(!empty($erros))
+        // {
+        //     $response = $this->response->json($erros, 400);
+        //     echo $response;
+        //     return;
+        // }
+        
+        // $data = $this->request->input();
+        // $result = User::insert($data);
+        // if($result)
+        // {
+        //     $response = $this->response->json($result, 201);
+        //     echo $response;
+        // }else{
+        //     $response = $this->response->json([
+        //         'status' => false,
+        //         'message' => 'User wasn`t registed! Try later!'
+        //     ], 201);
+        //     echo $response;
+        // }
     }
 }
