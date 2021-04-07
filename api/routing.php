d6ff6684402afc75707f1660d0864636565dc730
Aleksandra <a0993715642@gmail.com>
2021-04-07 21:53:56 +0300

    add new route

--- api/routing.php
+++ api/routing.php
@@ -3,8 +3,11 @@
 use Api\Core\Classes\Router;
 
 Router::get('/', 'Api\Classes\Controllers\MainController@index');
+
 Router::get('/signup', 'Api\Classes\Controllers\RegisterController@index');
 Router::post('/signup', 'Api\Classes\Controllers\RegisterController@signup');
 Router::get('/signin', 'Api\Classes\Controllers\LoginController@index');
 Router::post('/signin', 'Api\Classes\Controllers\LoginController@signin');
+
+Router::get('/profile', 'Api\Classes\Controllers\ProfileController@index');
 
