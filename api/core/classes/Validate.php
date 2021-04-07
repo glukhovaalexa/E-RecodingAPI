e7752ff5de67d15be640f61b8f1196dcf4d76909
Aleksandra <a0993715642@gmail.com>
2021-04-07 21:59:07 +0300

    valid

--- api/core/classes/Validate.php
+++ api/core/classes/Validate.php
@@ -6,17 +6,47 @@ use Api\Core\Classes\Request;
 class Validate {
     
     public $request;
+    public $errors = [];
     public function __construct($rules, Request $request)
     {
+        $this->request = $request;
         $this->validate($rules, $request);
     }
 
     public function validate($rules, $request)
     {
-        foreach($rules as $attribute => $rule)
+        foreach($rules as $attribute => $rules)
         {
-            var_dump($attribute, $rule);
-            exit;
+            foreach($rules as $rule)
+            {
+                if(is_array($rule))
+                {
+                    // var_dump(mb_strlen($request->input($attribute)) > $rule[1]);
+                    if($rule[0] === $request::MIN && mb_strlen($request->input($attribute)) < $rule[1])
+                    {
+                        $this->setError($attribute, $this->errors()[1]);
+                    }
+                }
+                else{
+                    if($rule === $request::REQUIRED && empty($request->input($attribute)))
+                    {
+                        $this->setError($attribute, $this->errors()[0]);
+                    }
+                }
+            }
         }
+    }
+
+    protected function setError($attribute, $message)
+    {
+        $this->errors['errors'][$attribute] = $message;
+    }
+
+    protected function errors()
+    {
+        return [
+            0 => 'This field is required',
+            1 => 'This field need to be longer then',
+        ];
     }
 }
