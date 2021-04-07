d72e711735ec3dbdcb9ef3e9bd84c67208ecb377
Aleksandra <a0993715642@gmail.com>
2021-04-07 21:54:43 +0300

    del method

--- api/classes/controllers/Controller.php
+++ api/classes/controllers/Controller.php
@@ -1,21 +1,18 @@
 <?php
 
 namespace Api\Classes\Controllers;
-// use Api\Core\Classes\Request;
+use Api\Core\Classes\Request;
 use Api\Core\Classes\Response;
-// use Api\Core\Classes\Validate;
 
 class Controller {
 
-    // public $request;
+    public $request;
     public $response;
-    public $validate;
 
     public function __construct()
     {
-        // $this->request = new Request;
+        $this->request = new Request;
         $this->response = new Response;
-        // $this->validate = new Validate;
     }
 
 }
