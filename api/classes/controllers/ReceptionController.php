a630c94443699d2b5aff83b699bb4d09bb8ef29c
Aleksandra <a0993715642@gmail.com>
2021-04-08 21:35:40 +0300

    add controller

---
+++ api/classes/controllers/ReceptionController.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Api\Classes\Controllers;
+use Api\Core\Classes\Request;
+use Api\Classes\Models\Reception;
+use Api\Classes\Models\User;
+
+class ReceptionController extends Controller {
+
+    /**
+     * get all receptions
+     * 
+     * return json
+     */
+    public function index()
+    {
+        $receptions = Reception::findMany(['user_id' => User::Auth()]);
+        if($receptions)
+        {
+            $response = $this->response->json($receptions, 201);
+            echo $response;
+        }else{
+            $response = $this->response->json([
+                'status' => false,
+                'message' => 'Try later!'
+            ], 404);
+            echo $response;
+        }
+    }
+
+    /**
+     * get one reception
+     * 
+     * return json
+     */
+    public function show(Request $request, $id)
+    {
+        $receptions = Reception::getOne($id);
+        if($receptions)
+        {
+            $response = $this->response->json($receptions, 201);
+            echo $response;
+        }else{
+            $response = $this->response->json([
+                'status' => false,
+                'message' => 'Not found!'
+            ], 404);
+            echo $response;
+        }
+    }
+
+    /**
+     * store reception
+     * 
+     * return json
+     */
+    public function store(Request $request)
+    {
+        $result = Reception::create($request->input());
+        if($result)
+        {
+            $response = $this->response->json($result, 201);
+            echo $response;
+        }else{
+            $response = $this->response->json([
+                'status' => false,
+                'message' => 'Try later!'
+            ], 404);
+            echo $response;
+        }
+    }
+
+    /**
+     * edit reception
+     * 
+     * return json
+     */
+    public function edit(Request $request)
+    {
+        $result = Reception::update($request->input());
+        if($result)
+        {
+            $response = $this->response->json($result, 200);
+            echo $response;
+        }else{
+            $response = $this->response->json([
+                'status' => false,
+                'message' => 'Not found!'
+            ], 404);
+            echo $response;
+        }
+    }
+
+    /**
+     * delete receprion
+     * 
+     * return json
+     */
+    public function delete(Request $request, $id)
+    {
+        $result = Reception::delete($id);
+        if($result)
+        {
+            $response = $this->response->json($result, 200);
+            echo $response;
+        }else{
+            $response = $this->response->json([
+                'status' => false,
+                'message' => 'Not found!'
+            ], 404);
+            echo $response;
+        }
+    }
+}
