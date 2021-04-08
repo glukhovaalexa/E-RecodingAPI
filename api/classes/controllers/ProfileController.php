b18b17f4a6bf40b9eb5c6d3d36d161ee31fe4207
Aleksandra <a0993715642@gmail.com>
2021-04-08 21:35:27 +0300

    add method

--- api/classes/controllers/ProfileController.php
+++ api/classes/controllers/ProfileController.php
@@ -1,19 +1,54 @@
 <?php
 
 namespace Api\Classes\Controllers;
+use Api\Core\Classes\Request;
+
 use Api\Classes\Models\User;
 use Api\Classes\Models\City;
+use Api\Classes\Models\Hospital;
+use Api\Classes\Models\Speciality;
+use Api\Classes\Models\Doctor;
 
 class ProfileController extends Controller {
 
     /**
-     * get request
-     * show home page
+     * get all cities, hospitals, specialities
+     * 
+     * return json
      */
     public function index()
     {
         $cities = City::getAll();
-        $response = $this->response->json($cities, 201);
+        $hospitals = Hospital::getAll();
+        $specialities = Speciality::getAll();
+        $response = $this->response->json([
+            'cities' => $cities,
+            'hospitals' => $hospitals,
+            'specialities' => $specialities
+        ], 200);
         echo $response;
+    }
+
+    /**
+     * handle search form 
+     * get doctors depend on citiy, hospital, speciality
+     * 
+     * return json
+     */
+    public function search(Request $request)
+    {
+        $doctors = Doctor::findMany($request->input());
+        if($doctors)
+        {
+            $response = $this->response->json($doctors, 200);
+            echo $response;
+        }else{
+            $response = $this->response->json([
+                'status' => false,
+                'message' => 'Not found!'
+            ], 404);
+            echo $response;
+        }
+
     }
 }
