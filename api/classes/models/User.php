26fa8af2d852509b22057f2a8c49abbdb37521ce
Aleksandra <a0993715642@gmail.com>
2021-04-20 23:35:16 +0300

    public protected

--- api/classes/models/User.php
+++ api/classes/models/User.php
@@ -8,9 +8,9 @@ class User extends Model{
     public string $lastname;
     public int $city_id;
     public string $phone;
-    protected string $email;
-    protected string $pass;
-    protected string $pass_rep;
+    public string $email;
+    public string $pass;
+    public string $pass_rep;
 
     /**
      * relationship
