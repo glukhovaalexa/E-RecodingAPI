8a9e3b48bf90e6d6894156762f88874357e78089
Aleksandra <a0993715642@gmail.com>
2021-04-21 00:43:41 +0300

    add login controller

--- api/classes/controllers/LoginController.php
+++ api/classes/controllers/LoginController.php
@@ -4,8 +4,12 @@ namespace Api\Classes\Controllers;
 use Api\Classes\Controllers\Controller;
 use Api\Core\Classes\Request;
 use Api\Classes\Requests\LoginRequest;
+use Api\Classes\Models\User;
+use Api\Core\Traits\HashMatches;
 
 class LoginController extends Controller {
+
+    use HashMatches;
 
     public function __construct()
     {
@@ -21,7 +25,7 @@ class LoginController extends Controller {
      */
     public function index()
     {
-        echo 'hi';
+        //
     }
 
     /**
@@ -30,6 +34,45 @@ class LoginController extends Controller {
      */
     public function signin(Request $request)
     {
-        ///
+        $errors = $this->request->validate->errors;
+        // if not path validateion
+        if(!empty($errors))
+        {
+            $response = $this->response->json($errors, 200);
+            echo $response;
+            return;
+        }
+        // if data is valida
+        $email = $this->request->input('email');
+        $pass = $this->request->input('pass');
+        $user = User::find(['email' => $email]);
+        if(!empty($user))
+        {
+            if($this->hashMatches($pass, $user[0]->pass)) 
+            {
+                echo $this->response->json([
+                    'status' => true,
+                    'message' => 'You are login',
+                    'user' => $user[0]
+                ], 200);
+                return;
+            }
+            $response = $this->response->json([
+                'status' => false,
+                'errors' => [
+                    'message' =>  'Login or passqord isn`t correct!'
+                ]
+            ], 200);
+            echo $response;
+            return;
+        }
+
+        echo $this->response->json([
+            'errors' => [
+                'message' => 'User not found, please sign up!'
+            ] 
+        ], 200);
+        return;
+
     }
 }
