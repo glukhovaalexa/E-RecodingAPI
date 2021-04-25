1b921a468d0311951f53010695c09cedfe42dfcc
Aleksandra <a0993715642@gmail.com>
2021-04-25 15:39:13 +0300

    add session

--- bootstrap.php
+++ bootstrap.php
@@ -6,6 +6,8 @@ header('Access-Control-Allow-Methods: *');
 header('Access-Control-Allow-Credentials: true');
 header('Content-Type: application/json');
 
+session_start();
+
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/api/routing.php';
 
