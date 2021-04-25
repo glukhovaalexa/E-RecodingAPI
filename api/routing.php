062d6c66937c0193889dbd4d5373c626a336f561
Aleksandra <a0993715642@gmail.com>
2021-04-25 15:39:47 +0300

    add logout

--- api/routing.php
+++ api/routing.php
@@ -11,6 +11,7 @@ Router::post('/signin', 'Api\Classes\Controllers\LoginController@signin');
 
 Router::get('/profile', 'Api\Classes\Controllers\ProfileController@index');
 Router::post('/profile', 'Api\Classes\Controllers\ProfileController@search');
+Router::post('/logout', 'Api\Classes\Controllers\ProfileController@logout');
 
 Router::get('/doctors', 'Api\Classes\Controllers\DoctorController@index');
 Router::get('/doctor/{id}', 'Api\Classes\Controllers\DoctorController@show');
