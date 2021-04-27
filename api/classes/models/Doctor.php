0e00d27d2c13f096720534a6d746424a1c4fd3c3
Aleksandra <a0993715642@gmail.com>
2021-04-27 21:28:35 +0300

    add relationship

--- api/classes/models/Doctor.php
+++ api/classes/models/Doctor.php
@@ -7,6 +7,54 @@ class Doctor extends Model{
     public string $name;
     public string $lastname;
     public int $hospital_id;
+    public int $city_id;
     public int $speciality_id;
     public string $roomNumber;
+
+    /**
+     * relationship
+     * 
+     * return obj
+     */
+    public function speciality() : Speciality
+    {
+        return $this->has(Speciality::class, $this, 'speciality_id');
+    }
+
+    /**
+     * relationship
+     * 
+     * return obj
+     */
+    public function hospital() : Hospital
+    {
+        return $this->has(Hospital::class, $this, 'hospital_id');
+    }
+
+    /**
+     * relationship
+     * 
+     * return obj
+     */
+    public function city() : City
+    {
+        return $this->has(City::class, $this, 'city_id');
+    }
+
+    public function related()
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'lastname' => $this->lastname,
+            'hospital_id' => $this->hospital_id,
+            'speciality_id' => $this->speciality_id,
+            'city_id' => $this->city_id,
+            'roomNumber' => $this->roomNumber,
+            'speciality' => $this->speciality(),
+            'hospital' => $this->hospital(),
+            'city' => $this->city(),
+        ];
+
+    }
 }
